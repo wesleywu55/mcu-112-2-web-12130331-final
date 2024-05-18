5f15d5e802b613d6618667a7042e313f5c367988
wesleywu55 <wesleywu5555@gmail.com>
2024-05-18 22:52:59 +0800

    feat(product): 利用 CurrencyPipe 顯示售價

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,4 +1,4 @@
-import { DatePipe } from '@angular/common';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 import {
   Component,
   EventEmitter,
@@ -11,7 +11,7 @@ import {
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, CurrencyPipe],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css',
 })
@@ -28,6 +28,7 @@ export class ProductCardComponent {
   isShowChange = new EventEmitter<boolean>();
 
   @Input() createDate!: Date;
+  @Input({ transform: numberAttribute }) price!: number;
 
   onSetDisplay(isShow: boolean): void {
     this.isShowChange.emit(isShow);
