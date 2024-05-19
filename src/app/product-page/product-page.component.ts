bd95f71997aeac3b19ddef87916dd7a3e57dd079
wesleywu55 <wesleywu5555@gmail.com>
2024-05-19 17:50:07 +0800

    feat(product): 使用 AsynPipe 顯示產品清單資料

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subject, startWith, switchMap } from 'rxjs';
 import { Product } from '../model/product';
@@ -8,33 +9,27 @@ import { ProductService } from '../services/product.service';
 @Component({
   selector: 'app-product-page',
   standalone: true,
-  imports: [ProductCardListComponent],
+  imports: [AsyncPipe, ProductCardListComponent],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.css',
 })
-export class ProductPageComponent implements OnInit {
+export class ProductPageComponent {
   router = inject(Router);
 
   private productService = inject(ProductService);
 
-  products!: Product[];
-
   private readonly refresh$ = new Subject<void>();
 
-  ngOnInit(): void {
-    this.refresh$
-      .pipe(
-        startWith(undefined),
-        switchMap(() => this.productService.getList())
-      )
-      .subscribe((products) => (this.products = products));
-  }
+  readonly products$ = this.refresh$.pipe(
+    startWith(undefined),
+    switchMap(() => this.productService.getList())
+  );
 
   onAdd(): void {
     const product = new Product({
       name: '書籍 Z',
       authors: ['作者甲', '作者乙', '作者丙'],
-      company: '博碩文件',
+      company: '博碩文化',
       isShow: true,
       imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
       createDate: new Date(),
