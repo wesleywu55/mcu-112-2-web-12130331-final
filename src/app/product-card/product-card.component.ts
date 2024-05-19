9b0166106700ce0abfe8551325a1cecdb21a5cb5
wesleywu55 <wesleywu5555@gmail.com>
2024-05-19 15:55:41 +0800

    feat(product-detail): 切換產品明細頁面實作

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,13 +1,5 @@
 import { CurrencyPipe, DatePipe } from '@angular/common';
-import {
-  Component,
-  EventEmitter,
-  HostBinding,
-  Input,
-  Output,
-  booleanAttribute,
-  numberAttribute,
-} from '@angular/core';
+import { Component, EventEmitter, HostBinding, Input, Output, booleanAttribute, numberAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
@@ -25,8 +17,6 @@ export class ProductCardComponent {
 
   @Input({ transform: booleanAttribute })
   isShow!: boolean;
-  @Output()
-  isShowChange = new EventEmitter<boolean>();
 
   @Input() createDate!: Date;
   @Input({ transform: numberAttribute }) price!: number;
@@ -34,7 +24,6 @@ export class ProductCardComponent {
   @HostBinding('class')
   class = 'product-card';
 
-  onSetDisplay(isShow: boolean): void {
-    this.isShowChange.emit(isShow);
-  }
+  @Output()
+  view = new EventEmitter<void>();
 }
